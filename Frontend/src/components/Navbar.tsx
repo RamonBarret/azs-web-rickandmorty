242e315db8c1b3a4b3b5aa678094ba82a827e7f6
RamonBarret <ramon_barreto_medrado@hotmail.com>
2024-02-24 19:02:50 -0300

    adding 'Rick and Morty' font style in the <h2 /> element of the <Navbar /> component

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar: React.FC = () => {
   return (
     <nav>
-      <img src="" alt="logo" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/1280px-Rick_and_Morty.svg.png" alt="logo" />
       <h1>Episodes</h1>
     </nav>
   );
